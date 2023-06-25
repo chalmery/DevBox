297cf00e31dd673d396a3810232fee291a5d9772
chalmery <chaochaoycc@gmail.com>
2023-06-25 16:39:04 +0800

    Json工具

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import SidebarItem from "@/app/SidebarItem";
-import JsonTools from "@/compontes/JsonTools";
-import JsonDiff from "@/compontes/JsonDiff";
+import JsonTools from "@/app/JsonTools";
+import JsonDiff from "@/app/JsonDiff";
 // 导入其他组件
 
 const App = () => {
-
   const dataSource = [
-     {"title":"Json工具","component":<JsonTools/>},
-     {"title":"Json比对","component":<JsonDiff/>},
-  ]
+    {title: "Json工具", component: <JsonTools />, type: "JsonTools" },
+    {title: "Json比对", component: <JsonDiff />, type: "JsonDiff" },
+  ];
 
-  const [activeComponent, setActiveComponent] = useState(<JsonTools/>);
+  const [activeComponent, setActiveComponent] = useState(<JsonTools />);
+  const [activeComponentType, setActiveComponentType] = useState("JsonTools");
 
-  const handleItemClick = (componentName) => {
+  const handleItemClick = (componentName, type) => {
     setActiveComponent(componentName);
+    setActiveComponentType(type);
   };
 
   return (
-    <div className="flex h-screen w-auto">
-      <div className="bg-gray-100">
-        {dataSource && dataSource.map((data)=>{
-          return <SidebarItem
-            title= {data.title}
-            onClick={() => handleItemClick(data.component)}
-          />
-        })}
-        {/* 添加其他工具栏项 */}
+    <div className="flex h-screen w-screen overflow-y-hidden">
+      <div className="bg-gray-100 w-1/8 h-full">
+        {dataSource &&
+          dataSource.map((data) => {
+            return (
+              <SidebarItem
+                key={data.type}
+                title={data.title}
+                onClick={() => handleItemClick(data.component, data.type)}
+                isActive={activeComponentType === data.type}
+              />
+            );
+          })}
       </div>
-      <div className="w-3/4 p-4">
-        {activeComponent}
-        {/* 添加其他组件的条件渲染 */}
-      </div>
+      <div className="flex-1 h-full">{activeComponent}</div>
     </div>
   );
 };
--- src/app/SidebarItem.jsx
+++ src/app/SidebarItem.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const SidebarItem = ({ title, onClick }) => {
+const SidebarItem = ({ title, onClick, isActive }) => {
   return (
     <button
-      className="flex items-center p-2 space-x-2 hover:bg-gray-200"
+      className={`flex w-full items-center p-2 space-x-2 hover:bg-blue-600 hover:text-white
+    ${isActive ? "bg-blue-500 text-white" : "bg-gray-100 text-black"}`}
       onClick={onClick}
     >
       {/* 可以根据需要添加图标 */}
